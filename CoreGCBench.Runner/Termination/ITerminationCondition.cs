0a7b140c6487f7f25d5fe5d370e3374add7c8216
Sean Gillespie <segilles@microsoft.com>
2016-09-22 15:42:26 -0700

    Support early termination of benchmark processes when a condition is met. This commit implements the 'Time' condition, where a process will be terminated if it has run for a period of time specified by the benchmark.

---
+++ CoreGCBench.Runner/Termination/ITerminationCondition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CoreGCBench.Runner.Termination
+{
+    /// <summary>
+    /// An ITerminationCondition is a class whose responsibility is to
+    /// determine, when asked, whether or not the running benchmark should terminate.
+    /// </summary>
+    public abstract class ITerminationCondition : IDisposable
+    {
+        /// <summary>
+        /// Asks this TerminationCondition whether or not it thinks it should terminate.
+        /// </summary>
+        /// <param name="proc">The Process that the termination condition should consider for termination.</param>
+        /// <returns>True if the process should be terminated.</returns>
+        public abstract bool ShouldTerminate(Process proc);
+
+        /// <summary>
+        /// Disposes this TerminationCondition. Does nothing unless overridden.
+        /// </summary>
+        public virtual void Dispose()
+        {
+            // do nothing.
+        }
+    }
+}
---
+++ CoreGCBench.Runner/Termination/NullTerminationCondition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CoreGCBench.Runner.Termination
+{
+    /// <summary>
+    /// NullTerminationCondition does nothing and always returns false for <see cref="ShouldTerminate"/>.
+    /// This TerminationCondition is used by the runner when no termination condition is specified.
+    /// </summary>
+    public sealed class NullTerminationCondition : ITerminationCondition
+    {
+        public override bool ShouldTerminate(Process proc)
+        {
+            return false;
+        }
+    }
+}
