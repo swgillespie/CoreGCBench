243268f87bb9973a1140143ed165686be8f35bf2
Sean Gillespie <segilles@microsoft.com>
2017-06-20 16:18:54 -0700

    Restore using nuget

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 @echo off
-dotnet restore
+dotnet restore src\CoreGCBench.Runner\CoreGCBench.Runner.csproj
+.nuget\nuget.exe restore CoreGCBench.Desktop.sln
 msbuild CoreGCBench.Core.sln /p:Configuration=Debug /p:Platform="Any CPU"
 msbuild CoreGCBench.Desktop.sln /p:Configuration=Debug /p:Platform="Any CPU"
